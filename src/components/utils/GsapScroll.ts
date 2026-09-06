d853a82fc728191baa986ee228c57446a8f6ae0a
Crusty-chirayu <chirayujayaswal7@gmail.com>
2026-09-06 20:32:39 +0530

    fix: stop character container from intercepting hero CTA clicks

--- src/components/utils/GsapScroll.ts
+++ src/components/utils/GsapScroll.ts
@@ -79,12 +79,7 @@ export function setCharTimeline(
         )
         .to(".about-section", { y: "30%", duration: 6 }, 0)
         .to(".about-section", { opacity: 0, delay: 3, duration: 2 }, 0)
-        .fromTo(
-          ".character-model",
-          { pointerEvents: "inherit" },
-          { pointerEvents: "none", x: "-12%", delay: 2, duration: 5 },
-          0
-        )
+        .to(".character-model", { x: "-12%", delay: 2, duration: 5 }, 0)
         .to(character.rotation, { y: 0.92, x: 0.12, delay: 3, duration: 3 }, 0)
         .to(neckBone!.rotation, { x: 0.6, delay: 2, duration: 3 }, 0)
         .to(monitor.material, { opacity: 1, duration: 0.8, delay: 3.2 }, 0)
